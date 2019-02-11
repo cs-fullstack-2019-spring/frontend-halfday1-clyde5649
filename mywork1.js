88ce0e2fdd4f0b7a3728ba111c36d2e7b4672d52
clyde5649 <clyde.bledsoe@yahoo.com>
2019-02-11 16:53:02 -0600

    still working on my page

---
+++ mywork1.js
@@ -0,0 +1,24 @@
+/* set background colors */
+
+$("#red").click(function () {
+    $("body").addClass("red")
+});
+
+
+
+
+
+$("#blue").click(function () {
+    $("body").addClass("blue")
+});
+
+
+
+
+$("#yellow").click(function () {
+    $("body").addClass("yellow")
+});
+
+
+var x = 1;
+var y = 1;
